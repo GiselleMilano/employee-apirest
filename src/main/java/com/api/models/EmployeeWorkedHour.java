806b159d77babe0f835a565b15dacae800d22315
Giselle <milanoregina8@gmail.com>
2023-08-04 10:21:40 -0300

    Added constructor and getters and setters methods in EmployeeWorkedHour Model

--- src/main/java/com/api/models/EmployeeWorkedHour.java
+++ src/main/java/com/api/models/EmployeeWorkedHour.java
@@ -20,4 +20,43 @@ public class EmployeeWorkedHour {
 
     @Column(name = "worked_date", columnDefinition = "DATE")
     private Date workedDate;
+
+    public EmployeeWorkedHour(Long id, Employee employee, Integer workedHours, Date workedDate) {
+        this.id = id;
+        this.employee = employee;
+        this.workedHours = workedHours;
+        this.workedDate = workedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Integer getWorkedHours() {
+        return workedHours;
+    }
+
+    public void setWorkedHours(Integer workedHours) {
+        this.workedHours = workedHours;
+    }
+
+    public Date getWorkedDate() {
+        return workedDate;
+    }
+
+    public void setWorkedDate(Date workedDate) {
+        this.workedDate = workedDate;
+    }
 }
