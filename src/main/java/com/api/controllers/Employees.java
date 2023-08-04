ef64d376c8a44f62999146d7e9616d20ee6377e9
Giselle <milanoregina8@gmail.com>
2023-08-04 17:33:38 -0300

    Added Employee Service in Employees Controller

--- src/main/java/com/api/controllers/Employees.java
+++ src/main/java/com/api/controllers/Employees.java
@@ -1,11 +1,16 @@
 package com.api.controllers;
 
 import com.api.models.Employee;
+import com.api.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
 public class Employees {
+
+    @Autowired
+    EmployeeService employeeService;
 
     @PostMapping("/add")
     public Employee addEmployee(@RequestBody Employee data) {
