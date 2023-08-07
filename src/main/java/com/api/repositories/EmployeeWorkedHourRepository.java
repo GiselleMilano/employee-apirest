8136d469ae6c07cd85e5598be916930fc8dccb6d
Giselle <milanoregina8@gmail.com>
2023-08-07 12:01:04 -0300

    New queries to make the search more personalized

--- src/main/java/com/api/repositories/EmployeeWorkedHourRepository.java
+++ src/main/java/com/api/repositories/EmployeeWorkedHourRepository.java
@@ -2,10 +2,15 @@ package com.api.repositories;
 
 import com.api.models.EmployeeWorkedHour;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface EmployeeWorkedHourRepository extends JpaRepository<EmployeeWorkedHour, Long> {
-    List<EmployeeWorkedHour> findByWorkedDate(String date);
+    @Query("SELECT ewh.workedDate FROM EmployeeWorkedHour ewh WHERE ewh.employeeId = :employeeId AND ewh.workedDate = :workedDate")
+    List<EmployeeWorkedHour> getWorkedHourByDateAndEmployeeId(Long employeeId, String workedDate);
+
+    @Query("SELECT SUM(ewh.workedHours) FROM EmployeeWorkedHour ewh WHERE ewh.employeeId = :employeeId AND ewh.workedDate BETWEEN :startDate AND :endDate")
+    Integer getTotalWorkedHoursByDateRange(Long employeeId, String startDate, String endDate);
 }
