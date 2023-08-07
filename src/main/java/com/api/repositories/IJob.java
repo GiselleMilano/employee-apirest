e16e75b329054d32035bbed69a9ae2ffe8da53d9
Giselle <milanoregina8@gmail.com>
2023-08-06 22:53:41 -0300

    Method findByName was created in the Job interface - New method to find a job by name in the data base.

--- src/main/java/com/api/repositories/IJob.java
+++ src/main/java/com/api/repositories/IJob.java
@@ -4,5 +4,9 @@ import com.api.models.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IJob extends JpaRepository<Job, Long> {}
+public interface IJob extends JpaRepository<Job, Long> {
+    List<Job> findByName(String name);
+}
