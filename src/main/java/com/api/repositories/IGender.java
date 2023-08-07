aeb36997fb50c8a63c6c5da0ee59313b82421e42
Giselle <milanoregina8@gmail.com>
2023-08-06 22:55:28 -0300

    New method findByName in the Gender interface

--- src/main/java/com/api/repositories/IGender.java
+++ src/main/java/com/api/repositories/IGender.java
@@ -4,5 +4,9 @@ import com.api.models.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IGender extends JpaRepository<Gender, Long> {}
+public interface IGender extends JpaRepository<Gender, Long> {
+    List<Gender> findByName(String name);
+}
