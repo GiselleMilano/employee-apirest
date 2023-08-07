c2ea10e4809e19d561789b3ccc23c7259943a997
Giselle <milanoregina8@gmail.com>
2023-08-06 22:35:52 -0300

    DefaultResponse Class created. - I created a class to set the dafault controller response.

---
+++ src/main/java/com/api/responses/DefaultResponse.java
@@ -0,0 +1,22 @@
+package com.api.responses;
+
+public class DefaultResponse {
+    private Long id;
+    private Boolean success;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+}
