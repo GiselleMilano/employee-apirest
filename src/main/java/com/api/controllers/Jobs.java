2504c6c7e0154c9057febed7552bce8541e8e5ef
Giselle <milanoregina8@gmail.com>
2023-08-07 12:03:00 -0300

    New validation method to compare the dates. - get total mount payment in a date range method added.

--- src/main/java/com/api/controllers/Jobs.java
+++ src/main/java/com/api/controllers/Jobs.java
@@ -1,18 +1,48 @@
 package com.api.controllers;
 
+import com.api.models.Employee;
 import com.api.models.Job;
+import com.api.requests.TotalWorkedHoursRequest;
 import com.api.responses.DefaultResponse;
+import com.api.responses.TotalPaymentResponse;
+import com.api.services.EmployeeService;
+import com.api.services.EmployeeWorkedHourService;
 import com.api.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/jobs")
 public class Jobs {
 
+    private final EmployeeService employeeService;
+    private final JobService jobService;
+    private final EmployeeWorkedHourService employeeWorkedHourService;
+
     @Autowired
-    JobService jobService;
+    public Jobs(EmployeeService employeeService, JobService jobService, EmployeeWorkedHourService employeeWorkedHourService) {
+        this.employeeService = employeeService;
+        this.jobService = jobService;
+        this.employeeWorkedHourService = employeeWorkedHourService;
+    }
+
+    private Boolean validateStartAndEndDate(String startDate, String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate startDateParsed = LocalDate.parse(startDate, formatter);
+            LocalDate endDateParsed = LocalDate.parse(endDate, formatter);
+
+            if (startDateParsed.isBefore(endDateParsed)) {
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return false;
+    }
 
     @PostMapping("/add")
     public DefaultResponse addJob(@RequestBody Job data) {
@@ -36,8 +66,36 @@ public class Jobs {
         return defaultResponse;
     }
 
-    @GetMapping("/get/{id}")
-    public Optional<Job> getJobById(@PathVariable Long id) {
+    @PostMapping("/get/total-payment")
+    public TotalPaymentResponse getTotalPayment(@RequestBody TotalWorkedHoursRequest data) {
+        TotalPaymentResponse totalPaymentResponse = new TotalPaymentResponse();
+
+        totalPaymentResponse.setPayment(null);
+        totalPaymentResponse.setSuccess(false);
+
+        Optional<Employee> employee = employeeService.getEmployeeById(data.getEmployeeId());
+
+        if (employee.isPresent()) {
+            Optional<Job> job = jobService.getJobById(employee.get().getJobId());
+
+            if (job.isPresent()) {
+                if (validateStartAndEndDate(data.getStartDate(), data.getEndDate())) {
+                    Integer totalWorkedHours = employeeWorkedHourService.getTotalWorkedHoursByDateRange(data.getEmployeeId(), data.getStartDate(), data.getEndDate());
+
+                    if (totalWorkedHours != null) {
+                        totalPaymentResponse.setPayment(job.get().getSalary() * totalWorkedHours);
+                    }else {
+                        totalPaymentResponse.setPayment(0.0);
+                    }
+                    totalPaymentResponse.setSuccess(true);
+                    return totalPaymentResponse;
+                }
+            }
+        }
+        return totalPaymentResponse;
+    }
+
+    public Optional<Job> getJobById(Long id) {
         if (id != null) {
             return jobService.getJobById(id);
         }
