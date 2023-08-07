a12cfef0b16682369b1a0fcbbc6d2acb10a62461
Giselle <milanoregina8@gmail.com>
2023-08-06 22:42:42 -0300

    Job Service updated. - Get job by id method added. - Get job by name method added. - Create job method implemented.

--- src/main/java/com/api/services/JobService.java
+++ src/main/java/com/api/services/JobService.java
@@ -5,15 +5,28 @@ import com.api.models.Job;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class JobService {
-    @Autowired
-    IJob iJob;
 
-    public Job CreateJob(Job data) {
-        return null;
+    private final IJob iJob;
+
+    @Autowired
+    public JobService(IJob iJob) {
+        this.iJob = iJob;
     }
-    public Job SelectJob(Long jobId) {
-        return null;
+
+    public Job createJob(Job data) {
+        return iJob.save(data);
+    }
+
+    public Optional<Job> getJobById(Long id) {
+        return iJob.findById(id);
+    }
+
+    public List<Job> getJobByName(String name) {
+        return iJob.findByName(name);
     }
 }
