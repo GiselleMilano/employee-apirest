e070953098e2cd79b7c70a210689e6c80cc43a3e
Giselle <milanoregina8@gmail.com>
2023-08-06 23:04:34 -0300

    addJob method was implemented. - New method to get job by id added.

--- src/main/java/com/api/controllers/Jobs.java
+++ src/main/java/com/api/controllers/Jobs.java
@@ -1,12 +1,11 @@
 package com.api.controllers;
 
 import com.api.models.Job;
+import com.api.responses.DefaultResponse;
 import com.api.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/jobs")
@@ -16,8 +15,32 @@ public class Jobs {
     JobService jobService;
 
     @PostMapping("/add")
-    public Job addJob(@RequestBody Job data) {
-        Job job = new Job(data.getId(), data.getName(), data.getSalary());
-        return job;
+    public DefaultResponse addJob(@RequestBody Job data) {
+        DefaultResponse defaultResponse = new DefaultResponse();
+
+        defaultResponse.setId(null);
+        defaultResponse.setSuccess(false);
+
+        if ((data.getName() != null && !data.getName().isEmpty()) && (data.getSalary() != null && data.getSalary() > 0)) {
+            if (jobService.getJobByName(data.getName()).size() == 0) {
+                try {
+                    Job job = jobService.createJob(data);
+                    defaultResponse.setId(job.getId());
+                    defaultResponse.setSuccess(true);
+                    return defaultResponse;
+                }catch(Exception e) {
+                    return defaultResponse;
+                }
+            }
+        }
+        return defaultResponse;
+    }
+
+    @GetMapping("/get/{id}")
+    public Optional<Job> getJobById(@PathVariable Long id) {
+        if (id != null) {
+            return jobService.getJobById(id);
+        }
+        return null;
     }
 }
