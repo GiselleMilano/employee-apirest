564731cf59cdc035304cbccc21d85ba0a6418575
Giselle <milanoregina8@gmail.com>
2023-08-06 22:57:06 -0300

    New findByWorkedDate method in the interface

--- src/main/java/com/api/repositories/IEmployeeWorkedHour.java
+++ src/main/java/com/api/repositories/IEmployeeWorkedHour.java
@@ -3,6 +3,9 @@ package com.api.repositories;
 import com.api.models.EmployeeWorkedHour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface IEmployeeWorkedHour extends JpaRepository<EmployeeWorkedHour, Long> {}
+public interface IEmployeeWorkedHour extends JpaRepository<EmployeeWorkedHour, Long> {
+    List<EmployeeWorkedHour> findByWorkedDate(String date);
+}
