c747499f2d2219e2f5627921a86d8eaac08b6a73
Giselle <milanoregina8@gmail.com>
2023-08-06 23:05:40 -0300

    addGender method was implemented. - New get gender by id method added.

--- src/main/java/com/api/controllers/Genders.java
+++ src/main/java/com/api/controllers/Genders.java
@@ -1,12 +1,11 @@
 package com.api.controllers;
 
 import com.api.models.Gender;
+import com.api.responses.DefaultResponse;
 import com.api.services.GenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/genders")
@@ -16,8 +15,32 @@ public class Genders {
     GenderService genderService;
 
     @PostMapping("/add")
-    public Gender addGender(@RequestBody Gender data) {
-        Gender gender = new Gender(data.getId(), data.getName());
-        return gender;
+    public DefaultResponse addGender(@RequestBody Gender data) {
+        DefaultResponse defaultResponse = new DefaultResponse();
+
+        defaultResponse.setId(null);
+        defaultResponse.setSuccess(false);
+
+        if (data.getName() != null && !data.getName().isEmpty()) {
+            if (genderService.getGenderByName(data.getName()).size() == 0) {
+                try {
+                    Gender gender = genderService.createGender(data);
+                    defaultResponse.setId(gender.getId());
+                    defaultResponse.setSuccess(true);
+                    return defaultResponse;
+                }catch(Exception e) {
+                    return defaultResponse;
+                }
+            }
+        }
+        return defaultResponse;
+    }
+
+    @GetMapping("/get/{id}")
+    public Optional<Gender> getGenderById(@PathVariable Long id) {
+        if (id != null) {
+            return genderService.getGenderById(id);
+        }
+        return null;
     }
 }
